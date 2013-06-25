c8d74866adbd0b4845dfc799b9aa8d9af8d6e056
Derek Chiang <derekchiang93@gmail.com>
2013-06-25 14:56:59 -0700

    Update version number

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject clojure-watch "0.1.5"
+(defproject clojure-watch "0.1.6"
   :description "A library for watching file system events."
   :url "https://github.com/derekchiang/Clojure-Watch"
   :license {:name "WTFPL"
             :url "http://www.wtfpl.net/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojure-contrib "LATEST"]
-                 [org.clojure/core.match "LATEST"]])
+                 [org.clojure/clojure-contrib "LATEST"]])
 
