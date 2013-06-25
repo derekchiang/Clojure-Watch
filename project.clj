18081927671d19b9ff541523c092b127576b0727
Derek Chiang <derekchiang93@gmail.com>
2013-06-25 01:05:02 -0700

    Update license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clojure-watch "0.1.0"
   :description "A library for watching file system events."
   :url "https://github.com/derekchiang/Clojure-Watch"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "WTFPL"
+            :url "http://www.wtfpl.net/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.match "LATEST"]])
 
