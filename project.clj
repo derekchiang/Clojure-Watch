c9f3bfdfc11cf2af9f503900faa105050ec5350b
Derek Chiang <derekchiang93@gmail.com>
2013-06-25 00:50:18 -0700

    Fix project file

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject clojure-watch "0.1.0-SNAPSHOT"
+(defproject clojure-watch "0.1.0"
   :description "A library for watching file system events."
   :url "https://github.com/derekchiang/Clojure-Watch"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojure-contrib "LATEST"]
                  [org.clojure/core.match "LATEST"]])
 
