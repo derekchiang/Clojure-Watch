9206a07d0c12ccb661d703c590fd5ac23b5a82f3
Derek Chiang <derekchiang93@gmail.com>
2016-11-28 00:58:12 -0800

    increment version again since i accidentally pushed some wrong code

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-watch "0.1.12"
+(defproject clojure-watch "0.1.13"
   :description "A library for watching file system events."
   :url "https://github.com/derekchiang/Clojure-Watch"
   :license {:name "WTFPL"
